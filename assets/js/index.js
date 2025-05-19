866a7a26fb0494be6989e83a24d03c378e1b24e1
Ishaya Tiseh <161091725+Sirdrkool@users.noreply.github.com>
2025-05-19 14:24:52 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const Menu = document.querySelector(".menu");
+function toggleMenu(){
+    Menu.classList.toggle("menuHide");
+}
+const Search = document.querySelector('.hide');
+function toggleSearch(){
+    Search.classList.toggle('hideHidden');
+}
